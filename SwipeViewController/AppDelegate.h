b15ab71e5cff0f628adc776297252809f5db7a4f
yuelin <yuelin_0@163.com>
2016-04-14 16:18:26 +0800

    Initial Commit

---
+++ SwipeViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SwipeViewController
+//
+//  Created by yl on 16/4/14.
+//  Copyright © 2016年 yl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
